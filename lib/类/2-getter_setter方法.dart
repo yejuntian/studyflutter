a7a9c4b3c83a9d0e87952a62a604012890d94884
tianyejun <tianyejun@adsconflux.com>
2025-05-08 14:31:24 +0800

    类- (+-*/)四种操作符运算.dart

--- lib/类/2-getter_setter方法.dart
+++ lib/类/2-getter_setter方法.dart
@@ -10,6 +10,19 @@ class Point {
   set x(int? x) {
     _x = x;
   }
+
+  //操作符重载,定义"+"操作符
+  Point operator +(Point other) {
+    var point = Point();
+    point._x = (_x! + other._x!);
+    return point;
+  }
+
+  //随意重载运算符返回值类型
+
+  String operator -(int value) {
+    return "${_x! - value}";
+  }
 }
 
 void main() {
@@ -23,4 +36,18 @@ void main() {
   //set
   var y = point.y;
   print("point.x = $x point.y = $y");
+
+  print("************操作符重载操作************");
+  var point1 = Point();
+  point1._x = 10;
+  var point2 = Point();
+  point2._x = 2;
+  var point3 = point1 + point2;
+  print("point3.x = ${point3._x}");
+
+  point3 = point1 + point2 + point3;
+  print("point3.x = ${point3._x}");
+
+  var result = point1 - 1;
+  print("result = $result");
 }
