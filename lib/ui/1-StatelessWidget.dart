2da4c3a2ff776d1b399a3d399d7aff9da504dacc
tianyejun <tianyejun@adsconflux.com>
2025-06-04 17:14:28 +0800

    1-StatelessWidget.dart和2-StatelessfulWidget.dart 补充说明

--- lib/ui/1-StatelessWidget.dart
+++ lib/ui/1-StatelessWidget.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/**
+    Stateless Widget
+
+    StatelessWidget用于不需要维护状态的场景，它通常在`build`方法中通过嵌套其它Widget来构建UI，
+    在构建过程中会递归的构建其嵌套的Widget。
+
+    BuildContext`表示构建widget的上下文，它是操作widget在树中位置的一个句柄，
+    它包含了一些查找、遍历当前Widget树的一些方法。每一个widget都有一个自己的context对象。
+ */
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +19,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  //在build方法中通过嵌套其它Widget来构建UI，在构建过程中会递归的构建其嵌套的Widget
   Widget build(BuildContext context) {
+    //嵌套 MaterialApp：封装了应用程序实现Material Design所需要的一些widget
     return MaterialApp(
+        //标题,显示在recent时候的标题
+        title: "Widget演示",
+        //主页面
+        //Scaffold : Material Design布局结构的基本实现。
         home: Scaffold(
-      appBar: AppBar(title: const Text("哈哈😄")),
-      body: const Center(child: Text("我很帅")),
-    ));
+          //android中的ToolBar/ActionBar
+          appBar: AppBar(title: const Text("哈哈😄")),
+          body: const Center(child: Text("我很帅")),
+        ));
   }
 }
--- lib/ui/2-StatelessfulWidget.dart
+++ lib/ui/2-StatelessfulWidget.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/**
+    （1）
+    StatelessWidget和StatefulWidget是flutter的基础组件，日常开发中自定义`Widget`都是选择继承这两者之一。也是在往后的开放中，我们最多接触的Widget：
+
+    StatelessWidget：无状态的，展示信息，面向那些始终不变的UI控件；
+
+    StatefulWidget：有状态的，可以通过改变状态使得 UI 发生变化，可以包含用户交互(比如弹出一个 dialog)。
+
+    （2）
+    StatefulWidget是动态的，添加了一个新的接口`createState()`用于创建和Stateful widget相关的状态`State`，
+    它在Stateful widget的生命周期中可能会被多次调用。
+
+    当State被改变时，可以手动调用其`setState()`方法通知Flutter framework状态发生改变，
+    Flutter framework在收到消息后，会重新调用其`build`方法重新构建widget树，从而达到更新UI的目的。
+
+ **/
 void main() {
   runApp(const MyApp());
 }
@@ -16,11 +32,17 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   String data = "我很帅";
 
+  //当Widget第一次插入到Widget树时会被调用initState()方法，对于每一个State对象，Flutter framework只会调用一次该回调
+  @override
+  void initState() {
+    debugPrint("只会调用一次改回调！");
+  }
+
   MyAppState() {
     Future.delayed(const Duration(seconds: 3)).then((value) {
       data = "你说的对，我很帅！";
 
-      //用来修改状态，刷新UI，重新调用build方法
+      //用来修改状态，刷新UI，会重新调用build方法更新UI
       setState(() {});
     });
 
