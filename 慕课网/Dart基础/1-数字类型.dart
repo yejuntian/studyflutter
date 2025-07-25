acda3f75d921d89375da68939f0d24316032c102
tianyejun <tianyejun@adsconflux.com>
2025-07-25 11:48:00 +0800

    [Feature] Dart String类型

--- 慕课网/Dart基础/1-数字类型.dart
+++ 慕课网/Dart基础/1-数字类型.dart
@@ -11,9 +11,11 @@ class _DataTypeState extends State<DataType> {
   @override
   Widget build(BuildContext context) {
     numType();
+    stringType();
     return Container(child: const Text("常用数据类型请查看控制台输出"));
   }
 
+  //数字类型
   void numType() {
     num num1 = -1.0; //是数字类型的父类
     num num2 = 2; //是数字类型的父类
@@ -22,8 +24,33 @@ class _DataTypeState extends State<DataType> {
     double d1 = 1.68; //双精度
 
     print("num:$num1 num2:$num2 int:$int1 double:$d1");
-    print(  num1.abs()); //求绝对值
+    print(num1.abs()); //求绝对值
     print(num1.toInt()); //转成int类型
     print(num1.toDouble()); //转成Double类型
+  }
+
+  //字符串
+  void stringType() {
+    //字符串定义
+    String str1 = '字符串', str2 = "双引号";
+
+    //字符串拼接
+    String str3 = 'str1:$str1 str2:$str2';
+
+    //字符串拼接
+    String str4 = 'str1:' + str1 + ' str2:' + str2;
+
+    print(str3);
+    print(str4);
+
+    //常用方法
+    String str5 = "常用数据类型，请看控制台输出";
+
+    //常用方法
+
+    //字符串截取
+    print(str5.substring(1, 5));
+    //获取指定字符串位置
+    print(str5.indexOf("类型"));
   }
 }
