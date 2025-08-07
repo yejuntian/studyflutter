dbbe9f560814bb8e6e459358e241340c833c7b4e
tianyejun <tianyejun@adsconflux.com>
2025-08-07 17:22:41 +0800

    [Feature] 抽取Banner到单独的Widget

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:studyflutter/widget/HomePageBanner.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,7 +19,6 @@ class _HomePageState extends State<HomePage>
     "http://pages.ctrip.com/hotel/201811/jdsc_640es_tab1.jpg",
     "https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg"
   ];
-  int _currentBannerIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -27,80 +27,17 @@ class _HomePageState extends State<HomePage>
     return Scaffold(
       //SafeArea 会根据设备的 状态栏、高度、底部手势区域 自动调整内边距,可以保证你的轮播图（或任何 widget）不会被系统 UI 遮挡
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  CarouselSlider(
-                    carouselController: _buttonCarouselController,
-                    options: CarouselOptions(
-                      onPageChanged:
-                          (int index, CarouselPageChangedReason reason) {
-                        _buttonCarouselController.jumpToPage(index);
-                        setState(() {
-                          _currentBannerIndex = index;
-                        });
-                      },
-                      initialPage: _currentBannerIndex,
-                      // ⚠️ 设置为 false，避免缩放效果带来的边距
-                      enlargeCenterPage: false,
-                      // ✅ 关键设置：让每页占满屏幕宽度
-                      viewportFraction: 1.0,
-                      aspectRatio: 16 / 9,
-                      autoPlay: true,
-                      autoPlayInterval: const Duration(seconds: 3),
-                    ),
-                    items: _getBannerImageList(),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    left: 0,
-                    right: 0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: _getBannerIndicator(),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                HomePageBanner(bannerList: bannerList),
+              ],
+            )
+          ],
         ),
       ),
     );
-  }
-
-  //获取轮播图图片列表
-  List<Widget> _getBannerImageList() {
-    return bannerList.map((imageUrl) {
-      return SizedBox(
-        height: 160,
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.fill,
-        ),
-      );
-    }).toList();
-  }
-
-//获取轮播指示器
-  List<Widget> _getBannerIndicator() {
-    return bannerList.asMap().entries.map((entry) {
-      return GestureDetector(
-        onTap: () => _buttonCarouselController.animateToPage(entry.key),
-        child: Container(
-          width: 8.0,
-          height: 8.0,
-          margin: const EdgeInsets.symmetric(horizontal: 4.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white
-                .withOpacity(_currentBannerIndex == entry.key ? 0.9 : 0.4),
-          ),
-        ),
-      );
-    }).toList();
   }
 
   @override
---
+++ lib/widget/HomePageBanner.dart
@@ -0,0 +1,90 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+//首页轮播图
+class HomePageBanner extends StatefulWidget {
+  final List<String> bannerList;
+
+  const HomePageBanner({
+    required this.bannerList,
+    super.key,
+  });
+
+  @override
+  State<HomePageBanner> createState() => _HomePageBannerState();
+}
+
+class _HomePageBannerState extends State<HomePageBanner> {
+  int _currentBannerIndex = 0;
+  final CarouselSliderController _buttonCarouselController =
+      CarouselSliderController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CarouselSlider(
+          carouselController: _buttonCarouselController,
+          options: CarouselOptions(
+            onPageChanged: (int index, CarouselPageChangedReason reason) {
+              _buttonCarouselController.jumpToPage(index);
+              setState(() {
+                _currentBannerIndex = index;
+              });
+            },
+            initialPage: _currentBannerIndex,
+            // ⚠️ 设置为 false，避免缩放效果带来的边距
+            enlargeCenterPage: false,
+            // ✅ 关键设置：让每页占满屏幕宽度
+            viewportFraction: 1.0,
+            aspectRatio: 16 / 9,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 3),
+          ),
+          items: _getBannerImageList(),
+        ),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _getBannerIndicator(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  //获取轮播图图片列表
+  List<Widget> _getBannerImageList() {
+    return widget.bannerList.map((imageUrl) {
+      return SizedBox(
+        height: 160,
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.fill,
+        ),
+      );
+    }).toList();
+  }
+
+//获取轮播指示器
+  List<Widget> _getBannerIndicator() {
+    return widget.bannerList.asMap().entries.map((entry) {
+      return GestureDetector(
+        onTap: () => _buttonCarouselController.animateToPage(entry.key),
+        child: Container(
+          width: 8.0,
+          height: 8.0,
+          margin: const EdgeInsets.symmetric(horizontal: 4.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white
+                .withOpacity(_currentBannerIndex == entry.key ? 0.9 : 0.4),
+          ),
+        ),
+      );
+    }).toList();
+  }
+}
